9fae93ff9cf96cb24609b10c5086a7b7976ed86d
Erik Hanchett <icystorm@gmail.com>
2016-07-12 11:14:42 -0400

    updated files

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	choice: null,
+	actions: {
+		selectChoice(choice){
+			this.set('choice',choice);
+		}
+	},
 	chartOptions: {
 		scales: {
 			yAxes: [{
@@ -10,19 +16,24 @@ export default Ember.Controller.extend({
 			}],
 		},
 	},
-	chartData: Ember.computed('model', function() {
+	chartData: Ember.computed('model','choice', function() {
 		let counts = {};
-		let arr = this.get('model').mapBy('whatIsYourAge');
+		let choice = this.get('choice') || 'sharedCommunityGarden';
+		let arr = this.get('model').mapBy(choice);
 		for(var i = 0; i< arr.length; i++) {
 			    var num = arr[i];
 			        counts[num] = counts[num] ? counts[num]+1 : 1;
 		}
-		let fin = Object.keys(counts).map(val => counts[val]);
+		debugger;
+		let fin = [counts['-5'], counts['-4'], counts['-3'], counts['-2'], counts['-1'],
+					counts['0'], counts['1'], counts['2'], counts['3'], counts['4'], counts['5']
+					];
 		return {
-			labels: this.get('model').mapBy('whatIsYourAge').uniq(),
+			//labels: this.get('model').mapBy('sharedCommunityGarden').uniq(),
+			labels: [-5,-4,-3,-2,-1,0,1,2,3,4,5],
 			datasets: [
 			{
-				label: 'breakdown by age',
+				label:choice,
 				data:fin,
 			},
 			],
--- app/models/survey.js
+++ app/models/survey.js
@@ -3,8 +3,14 @@ import attr from 'ember-data/attr';
 // import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
-  whatIsYourZipCode: attr('number'),
- howLongHaveYouLivedInReno: attr('string'),
-areYouARegisteredVoter: attr('string'),
-whatIsYourAge: attr('string')
+	   whatIsYourZipCode: attr('number'),
+	   howLongHaveYouLivedInReno: attr('string'),
+	   areYouARegisteredVoter: attr('string'),
+	   whatIsYourAge: attr('string'),
+	   sharedCommunityGarden: attr('number'),
+	   highSpeedInternet: attr('number'),
+	   lowNoiseLevels: attr('number'),
+	   proximityToFriendsFamily: attr('number'),
+	   publicWiFi: attr('number'),
+	   publicArt: attr('number')
 });
